7a5e45b7841c620cdaffd6041a5657826fc07ce6
Ess <pooster@gmail.com>
2014-09-15 18:51:01 -0400

    Fleshed out Linesman.within_bounds?

--- lib/linesman.rb
+++ lib/linesman.rb
@@ -9,8 +9,20 @@ module Linesman
 
     raise ArgumentError.new('a block is required') if block.nil?
 
-    
-  end
+    exactly = options[:exactly]
+    min = options[:min]
+    max = options[:max]
 
-  
+    under_scrutiny = block.call
+
+    if exactly
+      return under_scrutiny == exactly
+    elsif min && max
+      return under_scrutiny >= min && under_scrutiny <= max
+    elsif min
+      return under_scrutiny >= min
+    else
+      return under_scrutiny <= max
+    end
+  end
 end
--- spec/linesman_spec.rb
+++ spec/linesman_spec.rb
@@ -4,7 +4,7 @@ require 'linesman'
 describe Linesman do
   let(:klass) {Linesman}
 
-  describe %(.within_bounds?) do
+  describe '.within_bounds?' do
     it 'requires :exactly or some combination of :min and :max' do
       expect {klass.within_bounds?() {}}.to raise_error(ArgumentError)
       expect {klass.within_bounds?(exactly: 1) {1}}.not_to raise_error
@@ -16,6 +16,61 @@ describe Linesman do
     it 'requires a block' do
       expect {klass.within_bounds?(exactly: 1)}.to raise_error(ArgumentError)
       expect {klass.within_bounds?(exactly: 1) {0}}.not_to raise_error
+    end
+
+    context 'with :exactly specified' do
+      it 'ignores :min and :max' do
+        expect(klass.within_bounds?(exactly: 1, min: 2) {1}).to be_true
+        expect(klass.within_bounds?(exactly: 1, max: 0) {1}).to be_true
+      end
+
+      it 'is true if the block value is exactly as specified' do
+        expect(klass.within_bounds?(exactly: 1) {1}).to be_true
+      end
+
+      it 'is false if the block value is not exactly as specified' do
+        expect(klass.within_bounds?(exactly: 1) {0}).to be_false
+      end
+    end
+
+    context 'with :min specified' do
+      it 'is true if the block value is at least as specified' do
+        expect(klass.within_bounds?(min: 1) {1}).to be_true
+        expect(klass.within_bounds?(min: 1) {2}).to be_true
+      end
+
+      it 'is false if the block value is less than as specified' do
+        expect(klass.within_bounds?(min: 1) {0}).to be_false
+      end
+    end
+
+    context 'with :max specified' do
+      it 'is true if the block value is at most as specified' do
+        expect(klass.within_bounds?(max: 10) {10}).to be_true
+        expect(klass.within_bounds?(max: 10) {9}).to be_true
+      end
+
+      it 'is false if the block value is more than as specified' do
+        expect(klass.within_bounds?(max: 10) {11}).to be_false
+        expect(klass.within_bounds?(max: 10) {100}).to be_false
+      end
+    end
+
+    context 'with :min and :max specified' do
+      it 'is true if the block value is within the inclusive range min..max' do
+        min = 1
+        max = 10
+        (min..max).each do |value|
+          expect(klass.within_bounds?(min: min, max: max) {value}).to be_true
+        end
+      end
+
+      it 'is false if the block value is outside the inclusive range min..max' do
+        min = 10
+        max = 20
+        expect(klass.within_bounds?(min: min, max: max) {min - 1}).to be_false
+        expect(klass.within_bounds?(min: min, max: max) {max + 1}).to be_false
+      end
     end
   end
 end
